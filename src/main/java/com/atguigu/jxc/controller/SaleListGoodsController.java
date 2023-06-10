ce6636d150962feb3ea9a75a28500e363fac1fdd
yiyuan <1187998300@qq.com>
2023-06-10 19:54:35 +0800

    fix bugs

--- src/main/java/com/atguigu/jxc/controller/SaleListGoodsController.java
+++ src/main/java/com/atguigu/jxc/controller/SaleListGoodsController.java
@@ -3,10 +3,7 @@ package com.atguigu.jxc.controller;
 import com.atguigu.jxc.entity.SaleListGoods;
 import com.atguigu.jxc.service.SaleListGoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,8 +15,13 @@ public class SaleListGoodsController {
     @Autowired
     private SaleListGoodsService saleListGoodsService;
 
-    @PostMapping("goodsList/{saleListId}")
-    public Map<String , SaleListGoods>  saleListGood(@PathVariable("saleListId") Integer saleListId){
+    /**
+     * 销售单商品信息
+     * @param saleListId 出售id列表
+     * @return {@link Map}<{@link String} , {@link SaleListGoods}>
+     */
+    @PostMapping("/goodsList")
+    public Map<String , SaleListGoods>  saleListGood(@RequestParam("saleListId") Integer saleListId){
         SaleListGoods saleListGoods = saleListGoodsService.saleListGood(saleListId);
         HashMap<String, SaleListGoods> map = new HashMap<>();
         map.put("rows",saleListGoods);
